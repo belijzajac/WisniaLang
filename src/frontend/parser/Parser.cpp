2a9f31e785f4f0333451b303c07b1c52721fbfaa
belijzajac <tautvydas749@gmail.com>
2019-11-08 19:42:07 +0200

    Created general methods to perform on tokens

--- src/frontend/parser/Parser.cpp
+++ src/frontend/parser/Parser.cpp
@@ -1,1 +1,22 @@
 #include "Parser.h"
+#include "../lexer/Lexer.h"
+#include "../lexer/token/Token.h"
+#include "../lexer/token/TokenType.h"
+#include "../../utilities/Exception.h"
+
+Parser::Parser(const Lexer &lexer) {
+    tokens_ = lexer.getTokens();
+}
+
+bool Parser::has(const TokenType &token) {
+    return peek()->getType() == token;
+}
+
+// TODO: Instead of throwing exceptions, make an option to instead print error msg to STDERR
+void Parser::expect(const TokenType &token) {
+    if (peek()->getType() != token) {
+        throw Exception{"Expected " + TokenTypeToStr[token] + " but found " + TokenTypeToStr[peek()->getType()]};
+    } else {
+        consume();
+    }
+}
--- src/frontend/parser/Parser.h
+++ src/frontend/parser/Parser.h
@@ -1,6 +1,46 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-//...
+#include <memory>
+#include <vector>
+
+class Lexer;
+class Token;
+enum class TokenType;
+
+class Parser {
+    // Checks if the current token is of type `token`
+    bool has(const TokenType &token);
+
+    // Position of current token
+    size_t pos_ {0};
+
+    // Expects the following token to be of type `token`
+    void expect(const TokenType &token);
+
+public:
+    explicit Parser(const Lexer &lexer);
+    ~Parser() = default;
+
+    // Returns an instance of token at position pos
+    const std::shared_ptr<Token> &at(size_t pos) const {
+        return (pos >= tokens_.size() || pos < 0)? nullptr : tokens_.at(pos);
+    }
+
+    // Returns an instance of the current token
+    const std::shared_ptr<Token> &curr() const { return tokens_.at(pos_); }
+
+    // Returns an instance of the following token (peeks)
+    const std::shared_ptr<Token> &peek() const { return tokens_.at(pos_ + 1); }
+
+    // Consumes token (skips current token position by 1)
+    void consume() { pos_++; }
+
+    // Checks if we haven't reached the end of token stream
+    bool hasNext() const { return pos_ + 1 < tokens_.size(); }
+
+private:
+    std::vector<std::shared_ptr<Token>> tokens_;
+};
 
 #endif // PARSER_H
