6a933cd127bad2145cb3e19fafd9fa6527dab06d
belijzajac <tautvydas749@gmail.com>
2021-08-28 20:38:19 +0300

    UPD: Added code coverage reporting. UPD: Refactored CMakeLists files. UPD: Ubuntu yaml.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,9 @@
 include(FetchContent)
 
-if (unittests)
+if(UNIT_TESTS)
   FetchContent_Declare(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG        9741c42200b66abc708ee6da269a29c8bd912cee
   )
-
   FetchContent_MakeAvailable(googletest)
 endif()
--- cmake/opts.cmake
+++
@@ -1,4 +0,0 @@
-# The tests are not build by default. To build them, set the
-# unittests option to ON. You can do it by specifying the
-# -Dunittests=ON flag when running cmake.
-option(unittests "Build all of wisnia's unit tests." OFF)
