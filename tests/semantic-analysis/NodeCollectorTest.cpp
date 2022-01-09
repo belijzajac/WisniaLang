2d99c523f8e8e6b3a7bb6e0e1712fe814e68e03a
belijzajac <tautvydas749@gmail.com>
2022-01-09 18:38:38 +0200

    UPD: Move shared fixture to SemanticTestFixture.

--- tests/semantic-analysis/NodeCollectorTest.cpp
+++ tests/semantic-analysis/NodeCollectorTest.cpp
@@ -1,81 +1,12 @@
-#include <gtest/gtest.h>
 // Wisnia
 #include "Lexer.h"
 #include "Parser.h"
 #include "AST.h"
 #include "NodeCollector.h"
+#include "SemanticTestFixture.h"
 
 using namespace Wisnia;
-
-class NodeCollectorFixture : public testing::Test {
-  const std::string program = R"(
-  class Foo {
-    bool is_fifteen{false};
-    float number = 5.0;
-    int digit = 123456;
-
-    fn simple_operations () -> void {
-       float a = number + 7.75;
-       for (int i = 0; i < 5; i = i + 1) {
-         a = a + 1.45;
-         if (a == 15.0) {
-           is_fifteen = true;
-           break;
-         }
-       }
-       while (digit < 150000) {
-         a = a - 1.33;
-         digit = digit * 2;
-         print a; print digit;
-       }
-     }
-  }
-
-  fn output_hello (do_output : bool) -> string {
-    if (do_output) { return "hello"; }
-    return "bye";
-  }
-
-  fn main (argc : int, argv : string) -> int {
-    bool correct = (argc == 5);
-    string answer = output_hello(correct);
-    void fooPtr = new Foo{};
-
-    while (true) {
-      if (correct) {
-        bool yes = false;
-        answer = output_hello(yes);
-      }
-      elif (argc < 5) {
-        answer = output_hello(false);
-      }
-      elif (argc > 7) {
-        continue;
-      }
-      else {
-        break;
-      }
-    }
-
-    return 5;
-  }
-  )";
-
- public:
-  NodeCollectorFixture() {
-    std::istringstream iss{program};
-    lexer = std::make_unique<Lexer>(iss);
-    parser = std::make_unique<Parser>(*lexer);
-    root = parser->parse();
-  }
-
- protected:
-  std::unique_ptr<Lexer> lexer{};
-  std::unique_ptr<Parser> parser{};
-  std::unique_ptr<AST::Root> root{};
-};
-
-using NodeCollectorTest = NodeCollectorFixture;
+using NodeCollectorTest = SemanticTestFixture;
 
 TEST_F(NodeCollectorTest, ClassDef) {
   NodeCollector<AST::ClassDef> collector{};
---
+++ tests/semantic-analysis/SemanticTestFixture.h
@@ -0,0 +1,78 @@
+#ifndef SEMANTICTESTFIXTURE_H
+#define SEMANTICTESTFIXTURE_H
+
+#include <gtest/gtest.h>
+
+namespace Wisnia {
+
+class SemanticTestFixture : public testing::Test {
+  const std::string program = R"(
+  class Foo {
+    bool is_fifteen{false};
+    float number = 5.0;
+    int digit = 123456;
+
+    fn simple_operations () -> void {
+       float a = number + 7.75;
+       for (int i = 0; i < 5; i = i + 1) {
+         a = a + 1.45;
+         if (a == 15.0) {
+           is_fifteen = true;
+           break;
+         }
+       }
+       while (digit < 150000) {
+         a = a - 1.33;
+         digit = digit * 2;
+         print a; print digit;
+       }
+     }
+  }
+
+  fn output_hello (do_output : bool) -> string {
+    if (do_output) { return "hello"; }
+    return "bye";
+  }
+
+  fn main (argc : int, argv : string) -> int {
+    bool correct = (argc == 5);
+    string answer = output_hello(correct);
+    void fooPtr = new Foo{};
+
+    while (true) {
+      if (correct) {
+        bool yes = false;
+        answer = output_hello(yes);
+      }
+      elif (argc < 5) {
+        answer = output_hello(false);
+      }
+      elif (argc > 7) {
+        continue;
+      }
+      else {
+        break;
+      }
+    }
+
+    return 5;
+  }
+  )";
+
+ public:
+  SemanticTestFixture() {
+    std::istringstream iss{program};
+    lexer = std::make_unique<Lexer>(iss);
+    parser = std::make_unique<Parser>(*lexer);
+    root = parser->parse();
+  }
+
+ protected:
+  std::unique_ptr<Lexer> lexer{};
+  std::unique_ptr<Parser> parser{};
+  std::unique_ptr<AST::Root> root{};
+};
+
+}  // namespace Wisnia
+
+#endif  // SEMANTICTESTFIXTURE_H
