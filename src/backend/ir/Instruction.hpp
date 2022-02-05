5e1b6a605f18344ecfa5d1eb70d8dc9d50fa5b12
belijzajac <tautvydas749@gmail.com>
2022-02-05 23:08:05 +0200

    ADD: More IR operations for aarch64.

--- src/backend/ir/Instruction.hpp
+++ src/backend/ir/Instruction.hpp
@@ -10,24 +10,64 @@ namespace Basic {
 class Token;
 }  // namespace Basic
 
-enum class Operator {
-  MOV,
-  ADD,
-  SUB,
-  MUL,
+enum class Operation {
+  // arithmetic (each for int and float)
+  IADD,  FADD,
+  ISUB,  FSUB,
+  IMUL,  FMUL,
+  IDIV,  FDIV,
+  // comparison (each for int and float)
+  IEQ,   FEQ,
+  ILT,   FLT,
+  ILE,   FLE,
+  IGT,   FGT,
+  IGE,   FGE,
+  INE,   FNE,
+  // logical
+  NOT,
+  AND,
+  OR,
+  // miscellaneous
+  MOV,  // move to/from register
+  JMP,  // unconditional jump
+  BR,   // conditional branch
+  CALL, // function invocation
+  RET,  // function return
+  PRNT, // output values to the console
+  NOP   // do nothing
 };
 
-static inline std::unordered_map<Operator, std::string> Operator2Str {
-  {Operator::MOV, "<-"},
-  {Operator::ADD, "+"},
-  {Operator::SUB, "-"},
-  {Operator::MUL, "*"},
+static inline std::unordered_map<Operation, std::string> Operator2Str {
+  // arithmetic (each for int and float)
+  {Operation::IADD, "+"}, {Operation::FADD, "+"},
+  {Operation::ISUB, "-"}, {Operation::FSUB, "-"},
+  {Operation::IMUL, "*"}, {Operation::FMUL, "*"},
+  {Operation::IDIV, "/"}, {Operation::FDIV, "/"},
+  // comparison (each for int and float)
+  {Operation::IEQ, "=="}, {Operation::FEQ, "=="},
+  {Operation::ILT, "<" }, {Operation::FLT, "<" },
+  {Operation::ILE, "<="}, {Operation::FLE, "<="},
+  {Operation::IGT, ">" }, {Operation::FGT, ">" },
+  {Operation::IGE, ">="}, {Operation::FGE, ">="},
+  {Operation::INE, "!="}, {Operation::FNE, "!="},
+  // logical
+  {Operation::NOT, "!" },
+  {Operation::AND, "&&"},
+  {Operation::OR,  "||"},
+  // miscellaneous
+  {Operation::MOV,  "<-"   },
+  {Operation::JMP,  "jmp"  },
+  {Operation::BR,   "br"   },
+  {Operation::CALL, "call" },
+  {Operation::RET,  "ret"  },
+  {Operation::PRNT, "print"},
+  {Operation::NOP,  "nop"  },
 };
 
 class Instruction {
  public:
   Instruction(
-    Operator op,
+    Operation op,
     const std::shared_ptr<Basic::Token> &target,
     const std::shared_ptr<Basic::Token> &arg1,
     const std::shared_ptr<Basic::Token> &arg2 = nullptr
@@ -36,7 +76,7 @@ class Instruction {
   void print() const;
 
  private:
-  Operator m_operator;
+  Operation m_operator;
   std::shared_ptr<Basic::Token> m_target;
   std::shared_ptr<Basic::Token> m_arg1;
   std::shared_ptr<Basic::Token> m_arg2;
