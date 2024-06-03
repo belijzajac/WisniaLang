dc329f6b943f387658a1591782a3f958de80501e
belijzajac <tautvydas749@gmail.com>
2024-06-03 22:49:24 +0300

    Update `GoogleTest` and `fmt` version

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,14 +6,14 @@ include(FetchContent)
 if(UNIT_TESTS)
   FetchContent_Declare(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        9741c42200b66abc708ee6da269a29c8bd912cee
+    GIT_TAG        v1.14.0
   )
   FetchContent_MakeAvailable(googletest)
 endif()
 
 FetchContent_Declare(fmt
   GIT_REPOSITORY https://github.com/fmtlib/fmt
-  GIT_TAG        8.0.1
+  GIT_TAG        10.2.1
 )
 FetchContent_MakeAvailable(fmt)
 
